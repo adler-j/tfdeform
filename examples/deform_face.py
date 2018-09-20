9a0acb33149e930605699a5a5de6c7fffce58778
Jonas Adler <jonasadl@kth.se>
2018-09-20 22:19:47 +0200

    Updates to example

--- examples/deform_face.py
+++ examples/deform_face.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 from scipy import misc
-import numpy as np
 import matplotlib.pyplot as plt
 import tfdeform
 
@@ -11,12 +10,11 @@ face = misc.face(gray=True)
 shape = face.shape
 face = tf.convert_to_tensor(face[None, ..., None].astype('float32'))
 
-from_total_offset = tfdeform.create_deformation_momentum(
-    shape=[1, *shape], std=100.0, distance=100.0, stepsize=0.01)
-face_deform = tfdeform.dense_image_warp(face, from_total_offset)
+offset = tfdeform.create_deformation_momentum(
+    shape=[1, *shape], std=50.0, distance=100.0, stepsize=0.05)
+face_deform = tfdeform.dense_image_warp(face, offset)
 
-result_face, result_deform = session.run(
-    [face, face_deform])
+result_face, result_deform = session.run([face, face_deform])
 
 plt.figure()
 plt.imshow(result_face[0, ..., 0], cmap='gray')
